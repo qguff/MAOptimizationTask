952a9a57077f30748bf362580ae4f10528fc4e39
дарья сюрмеева <dasasurmeeva@outlook.com>
2025-12-03 16:56:07 +0400

    nhjhh

--- MAOptimizationTask/MAOptimizationTask.cpp
+++ MAOptimizationTask/MAOptimizationTask.cpp
@@ -8,8 +8,10 @@ bool isPrimeBase(int n)
 {
     if (n < 2) return false;
     if (n == 2) return true;
-    for (int i = 3; i < n; i++)
-        if (n % i == 0) return false;
+    if (n % 2 == 0) return false;
+    int p = sqrt(n);
+    for (int i = 3; i < p; i++)
+        if (n % 2 == 0) return false;
     return true;
 }
 
